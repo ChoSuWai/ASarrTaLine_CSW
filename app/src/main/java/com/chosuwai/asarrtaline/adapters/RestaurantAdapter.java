accb044ae7eb3fbcc37be19d403b21ca2ff39f19
Unknown <chosuwai@gmail.com>
2018-07-06 00:58:19 +0630

    Add First Screen without Search Bar

---
+++ app/src/main/java/com/chosuwai/asarrtaline/adapters/RestaurantAdapter.java
@@ -0,0 +1,31 @@
+package com.chosuwai.asarrtaline.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chosuwai.asarrtaline.R;
+import com.chosuwai.asarrtaline.viewholders.RestaurantViewHolder;
+
+public class RestaurantAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.view_holder_offers, parent, false);
+        return new RestaurantViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
